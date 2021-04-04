cf3360466caad5c95a16afcb119a16bb592cbc52
Joseph Tomlinson <bizzclaw1@gmail.com>
2021-04-03 18:09:04 -0700

    disable pvp depending on gamestage

--- gamemode/core/sv_dac.lua
+++ gamemode/core/sv_dac.lua
@@ -8,3 +8,14 @@ function DAC:SyncGameStage(ply)
 		net.Broadcast()
 	end
 end
+
+hook.Add("EntityTakeDamage", "DAC.GameStage.Damage", function(ent, dmginfo)
+	local stage = DAC:GetGameStage()
+	local attacker = dmginfo:GetAttacker()
+	local data = stage:GetData()
+	if not data.pvp and attacker:IsPlayer() and ent:IsPlayer() then
+		attacker:ChatPrint("Damage is Disabled while the game stage is "..data.name)
+		dmginfo:ScaleDamage(0)
+		return true
+	end
+end)
